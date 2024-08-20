aed16a68b7550c7d613f95ea6b02639f92f982e3
Natanael Alves Fernandes <natanael.alves.fernandes@gmail.com>
2024-08-20 18:09:14 -0300

    style: formatar classe webfilter

--- src/main/java/com/educaguard/security/WebFilterConfiguration.java
+++ src/main/java/com/educaguard/security/WebFilterConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -24,53 +23,53 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class WebFilterConfiguration {
 
-	@Autowired
-	private InterceptorFilter interceptorFilter;
+    @Autowired
+    private InterceptorFilter interceptorFilter;
 
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.addAllowedOrigin("*"); // Permite todas as origens
-		configuration.addAllowedMethod("*"); // Permite todos os métodos HTTP
-		configuration.addAllowedHeader("*"); // Permite todos os cabeçalhos
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*"); // Permite todas as origens
+        configuration.addAllowedMethod("*"); // Permite todos os métodos HTTP
+        configuration.addAllowedHeader("*"); // Permite todos os cabeçalhos
 
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
 
-		return source;
-	}
+        return source;
+    }
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.cors().configurationSource(corsConfigurationSource()); // Aplica a configuração CORS
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource()); // Aplica a configuração CORS
 
-		http.csrf(AbstractHttpConfigurer::disable); // Desativa CSRF
-		http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Sem sessões
+        http.csrf(AbstractHttpConfigurer::disable); // Desativa CSRF
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // Sem sessões
 
-		http.authorizeHttpRequests(auth -> auth
-				// Permissões para outros endpoints
-				.requestMatchers(HttpMethod.POST, "/login/*").permitAll()
-				.requestMatchers(HttpMethod.POST, "/login/*/recfacial").permitAll()
-				.requestMatchers(HttpMethod.GET, "/email/confirmation/*").permitAll()
-				.requestMatchers(HttpMethod.GET, "/recover/recover-account/*").permitAll()
-				.requestMatchers(HttpMethod.POST, "/recover/new-password").permitAll()
-				.requestMatchers(HttpMethod.POST, "/user/new").permitAll()
-				.requestMatchers(HttpMethod.GET, "/user/find/*").hasAuthority(Roles.ROLE_USER.name())
-				.requestMatchers(HttpMethod.POST, "user/*/uploadImage").hasAuthority(Roles.ROLE_USER.name())
-				.anyRequest().authenticated());
+        http.authorizeHttpRequests(auth -> auth
+                // Permissões para outros endpoints
+                .requestMatchers(HttpMethod.POST, "/login/*").permitAll()
+                .requestMatchers(HttpMethod.POST, "/login/*/recfacial").permitAll()
+                .requestMatchers(HttpMethod.GET, "/email/confirmation/*").permitAll()
+                .requestMatchers(HttpMethod.GET, "/recover/recover-account/*").permitAll()
+                .requestMatchers(HttpMethod.POST, "/recover/new-password").permitAll()
+                .requestMatchers(HttpMethod.POST, "/user/new").permitAll()
+                .requestMatchers(HttpMethod.GET, "/user/find/*").hasAuthority(Roles.ROLE_USER.name())
+                .requestMatchers(HttpMethod.POST, "user/*/uploadImage").hasAuthority(Roles.ROLE_USER.name())
+                .anyRequest().authenticated());
 
-		http.addFilterBefore(this.interceptorFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(this.interceptorFilter, UsernamePasswordAuthenticationFilter.class);
 
-		return http.build();
-	}
+        return http.build();
+    }
 
-	@Bean
-	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
